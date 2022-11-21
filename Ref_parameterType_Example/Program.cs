c4de9d78c7f87782b97cc230e9b8b2d879504798
adurga-ariqt <adurga@ariqt.com>
2022-11-21 13:00:38 +0530

    Add project files.

---
+++ Ref_parameterType_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ref_parameterType_Example;
+
+Console.WriteLine("Hello, World!");
+RefType obj = new RefType();
+int a = 10;
+int b= 20;
+Console.WriteLine("before swaping the  value of x :"+a);
+Console.WriteLine("before swaping the value of y ;"+b);
+obj.Swap(ref a, ref b);
+Console.WriteLine("after swaping the  value of x :" + a);
+Console.WriteLine("after  swaping the  value of y :" + b);
+Console.WriteLine("________________________");
+int addition = 0;
+int multiplication;
+
+obj.Opetarions(2, 3, ref addition, out multiplication);
+Console.WriteLine("the value is :"+addition);
+Console.WriteLine("the value is :"+multiplication);
---
+++ Ref_parameterType_Example/RefType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_parameterType_Example
+{
+    public  class RefType
+    {
+        public void Swap( ref int x,ref  int y )
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        public void Opetarions( int num1,int num2,ref int addition,out int multiplication)
+        {
+            addition = num1 + num2;
+            multiplication = num1*num2;
+        }
+    }
+}
